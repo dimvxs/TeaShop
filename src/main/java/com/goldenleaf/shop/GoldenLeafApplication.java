431507e68b0457381180f1cf311b3a6f814e3bd2
dima <dzhosan777@gmail.com>
2025-10-16 23:17:17 +0200

    project created

---
+++ src/main/java/com/goldenleaf/shop/GoldenLeafApplication.java
@@ -0,0 +1,13 @@
+//package com.goldenleaf.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenLeafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldenleaf/shop/GoldenLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldenleaf.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
