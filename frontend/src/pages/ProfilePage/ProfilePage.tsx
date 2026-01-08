45b715634ff4f0ca23ab6d0cf915e714bd22d1ef
B0nsen <oitut5@gmail.com>
2026-01-08 15:43:31 +0200

    Profile fix

--- frontend/src/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage/ProfilePage.tsx
@@ -98,23 +98,24 @@ export default function ProfilePage() {
         email: ""
     }
 
-    if (!user) {
-        return <div style={{ padding: 40 }}>Вы не авторизованы</div>;
-    }
-
     const Sitems = [
         {
             id: 1,
             title: "Мой аккаунт",
             subtitle: [
-                { title: "Логин", desc: user.login },
-                { title: "Телефон", desc: user.mobile },
-                { title: "Email", desc: user.email },
+                { title: "Логин", desc: user!.login },
+                { title: "Телефон", desc: user!.mobile },
+                { title: "Email", desc: user!.email },
             ],
         },
     ];
 
-    const [items, setItems] = useState!(Sitems);
+    const [items, setItems] = useState(Sitems);
+
+    if (!user) {
+        return <div style={{ padding: 40 }}>Вы не авторизованы</div>;
+    }
+    
 
     const editItemValue = (text: string, title: string) => {
         setItems(prev =>
@@ -185,6 +186,31 @@ export default function ProfilePage() {
             ))}
 
             <ChangePassword />
+
+            {/*<div className = "auth-block">*/}
+            {/*    <button className="auth-button">*/}
+            {/*        <span className="auth-icon" />*/}
+            {/*        <div className = "auth-text-block">*/}
+            {/*            <span className="auth-text">Google</span>*/}
+            {/*            <span className="auth-text">Привя</span>*/}
+            {/*        </div>*/}
+            {/*    </button>*/}
+            {/*    <button className="auth-button">*/}
+            {/*        <span className="auth-icon" />*/}
+            {/*        <div className="auth-text-block">*/}
+            {/*            <span className="auth-text">Google</span>*/}
+            {/*            <span className="auth-text">Привя</span>*/}
+            {/*        </div>*/}
+            {/*    </button>*/}
+            {/*    <button className="auth-button">*/}
+            {/*        <span className="auth-icon" />*/}
+            {/*        <div className="auth-text-block">*/}
+            {/*            <span className="auth-text">Google</span>*/}
+            {/*            <span className="auth-text">Привя</span>*/}
+            {/*        </div>*/}
+            {/*    </button>*/}
+            {/*</div>*/}
+            
         </div>
     );
 }
